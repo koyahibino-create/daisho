682f84a83c9aac1fd85d3c6bf0004846ca06724d
koyahibino-create <koya.hibino@gmail.com>
2025-10-30 13:47:18 +0900

    更新

--- daisho-day1/script.js
+++ daisho-day1/script.js
@@ -1,15 +1,13 @@
-const helloBtn=document.getElementByld("helloBtn");
-const message=document.getElementByld("message");
+const helloBtn = document.getElementByld("helloBtn");
+const message = document.getElementByld("message");
 
 helloBtn.addEventListerner("click",()=> {
-    const hour=new Date().getHours();
-    let greeting="こんにちは！";
-
+    const hour = new Date().getHours();
+    let greeting = "こんにちは！";
     if (hour<12){
-        greeting="おはようございます！";
-    }else if (hour=>18){
-        greeting="こんばんは！";
+        greeting = "おはようございます！";
+    }else if (hour => 18){
+        greeting = "こんばんは！";
     }
-
-    message.textcontent='${greeting} GitHub Pages の公開に成功しました！';
+    message.textContent = `${greeting} GitHub Pages の公開に成功しました！`;
 });
